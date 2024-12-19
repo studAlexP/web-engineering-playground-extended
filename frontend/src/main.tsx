98b2cd40435cf5b2636fb22e45a8e2d5669285e1
Alexander Pruka <alexp@fedora.fritz.box>
2024-12-19 15:05:49 +0100

    chore: remove unused import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/style.css';
 import App from './App.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- root element is guaranteed to exist
