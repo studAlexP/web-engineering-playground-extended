5979e17c7d88e9c8cc3a60512b9e79b7df4a5e82
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2025-01-01 16:03:29 +0100

    feat: add AuthorInfo component

---
+++ frontend/src/components/AuthorInfo.tsx
@@ -0,0 +1,16 @@
+function AuthorInfo(): React.JSX.Element {
+  return (
+    <aside>
+      <h3>About the author</h3>
+      <p>
+        Evan Wild is an unemployed plumber from Doncaster, who has been really
+        "in to" wildlife since childhood. He once went to Chester Zoo on
+        holiday, but got ill after eating a doner kebab. He has never seen a
+        bear, but once read a Daily Mail article about them, and thinks they
+        sound cool.
+      </p>
+    </aside>
+  );
+}
+
+export default AuthorInfo;
