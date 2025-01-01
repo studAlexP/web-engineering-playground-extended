8659b18d86f4affed20372d9993a6f67f705fcd4
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2025-01-01 16:05:21 +0100

    feat: integrate article and related component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@ import './App.css';
 import Header from './components/Header.tsx';
 import Navigation from './components/Navigation.tsx';
 import Footer from './components/Footer.tsx';
-import Comment from './components/Comment.tsx';
+import Article from './components/Article.tsx';
+import Related from './components/Related.tsx';
 
 function App(): React.JSX.Element {
   return (
     <>
       <Header />
       <Navigation />
-      <Comment />
+      <main>
+        <Article />
+        <Related />
+      </main>
       <Footer />
     </>
   );
