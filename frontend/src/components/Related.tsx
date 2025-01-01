6807ba7f78ce100c053ffbc455ebe164f127af60
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2025-01-01 16:04:50 +0100

    feat: add related component

---
+++ frontend/src/components/Related.tsx
@@ -0,0 +1,27 @@
+function Related(): React.JSX.Element {
+  return (
+    <div className="secondary">
+      <h2>Related</h2>
+
+      <ul>
+        <li>
+          <a href="#">The trouble with Bees</a>
+        </li>
+        <li>
+          <a href="#">The trouble with Otters</a>
+        </li>
+        <li>
+          <a href="#">The trouble with Penguins</a>
+        </li>
+        <li>
+          <a href="#">The trouble with Octopi</a>
+        </li>
+        <li>
+          <a href="#">The trouble with Lemurs</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Related;
